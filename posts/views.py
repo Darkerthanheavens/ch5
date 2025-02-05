fbf73667dd5ff4f9ff4d8dc4448b169cc5d8ea21
sepehr <sepehrsgdt@gmail.com>
2025-02-05 11:03:14 +0330

    Second commit

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from posts.models import Post
+
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    template_name = 'posts/post_list.html'
+    queryset = Post.objects.all()
